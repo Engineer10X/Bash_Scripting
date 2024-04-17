5e49227e16470532d4dd227e7e74a0e989d7e42f
Nikhil Saini <104626649+Engineer10X@users.noreply.github.com>
2024-04-17 00:26:37 -0500

    Add files via upload
    Day2: Variable and Math function

---
+++ Day2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+PATH=/bin:$PATH
+
+myname="Engineer10X"
+
+echo "Hello, Mr.$myname"
+
+command=$(ls)
+now = $(/bin/date)
+
+echo "run the ls command using variable."
+echo $command
+echo "whats the date and time"
+echo $now
+
+echo "checking the environment variable USER"
+echo $USER
+
+
+
+echo "check environment variable"
+env
+
+echo "now perform some math function"
+
+echo "Addition"
+expr 30 + 20
+echo "Subtraction"
+expr 30-20
+echo "Division"
+expr 30/10
+echo "Multiplication"
+expr 30\*10
